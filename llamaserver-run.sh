6aaa7b5cb8c87d74c512865ac3205769af2702a1
h0ffy <45763853+h0ffy@users.noreply.github.com>
2025-07-08 21:26:47 +0200

    Create llamaserver-run.sh

---
+++ llamaserver-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## Llama CPP
+
+echo "Run server Llama in /opt/llama-cpp/bin/llama-server"
+
+cd /opt/llama-cpp/bin
+
+./llama-server -m jemma2.gguf \
+  --host 0.0.0.0 \
+  --port 8080 \
+  --ctx-size 4096 \
+  --threads $(nproc) \
+  --chat-template chatml \
+
